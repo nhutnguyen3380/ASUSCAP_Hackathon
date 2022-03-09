a1ffd97fe8f0829e64a415010de8c9e5dcef5b51
iamTanTan <70108451+iamTanTan@users.noreply.github.com>
2022-03-09 15:52:58 -0700

    front end updates

--- application/routes.py
+++ application/routes.py
@@ -27,28 +27,65 @@ def creating_order():
             #flash('Content is required!')
         else:
             #save item and redirect
-            associated_customer = User.query.filter_by(username=username).first()
+            try:
+                associated_customer = User.query.filter_by(username=username).first()
 
-            new_order = Order(user_id=associated_customer.id)
-            an_item = Item(text="sample item", order_id=new_order.id)
-            db.session.add(new_order)
-            db.session.add(an_item)
-            db.session.commit()
+                new_order = Order(user_id=associated_customer.id)
+                an_item = Item(text="sample item", order_id=new_order.id)
+                db.session.add(new_order)
+                db.session.add(an_item)
+                db.session.commit()
+                return redirect('/')
+            except:
+                return 'There was an error for the order creation process'
+    else:
+        return render_template('processing.html', orders=Order.query.filter(Order.iscomplete==False).all())
 
-            return render_template('index.html', users=User.query.all())
-
-    return render_template('processing.html', orders=Order.query.filter(Order.iscomplete==False).all())
+    
 
 
 def order_processing():
     return render_template('processing.html', orders=Order.query.filter(Order.iscomplete==False).all())
 
+def complete_an_order():
+    if request.method== 'POST':
+        #get orderid (value) from post request submission
+        orderid = request.form['Complete_Order']
+
+        try:
+            order = Order.query.filter(Order.id==orderid).first()
+        
+
+            if not order:
+                flash('Order does not seem to exist!')
+
+            user = User.query.filter(User.id==order.user_id).first()
+            print(user.id)
+
+            if not user:
+                flash('Customer is not found for order!')
+
+            if user.optedin == True:
+                #perform API call
+                print('API called here')
+
+            order.iscomplete = True
+            db.session.add(order)
+            db.session.commit()
+            return render_template('completed.html', complete_orders=Order.query.filter(Order.iscomplete==True).all())
+        except:
+            return 'There was an error for the order CONFIRMATION process'
+
+    else:
+        return render_template('processing.html', orders=Order.query.filter(Order.iscomplete==False).all())
+
 def complete_orders():
-    return render_template('completed.html')
+    return render_template('completed.html', complete_orders=Order.query.filter(Order.iscomplete==True).all())
 
 # register call back function upon visiting particular url
 routes = Blueprint("routes", __name__)
 routes.route('/')(index)
 routes.route("/create", methods=['POST'])(creating_order)
+routes.route('/confirm', methods=['POST'])(complete_an_order)
 routes.route("/processing")(order_processing)
 routes.route("/complete")(complete_orders)
