18166d980e8086334c2c5903ddeae072255a24c4
iamTanTan <70108451+iamTanTan@users.noreply.github.com>
2022-03-10 22:02:11 -0700

    update frontend

--- application/routes.py
+++ application/routes.py
@@ -5,7 +5,8 @@ from twilio.rest import Client
 import speech_recognition as sr
 import time 
 
-
+account_sid = 'ACb9b69fcad32c0d9fd02fea6c8fad4a70'
+auth_token = 'f56940ec6b359efaab4a7b5f77eb3adf'
 client = Client(account_sid, auth_token)
 
 # NOTICE: FLASK follows a MVT (Model-View-Template) like Architectural Design Pattern
@@ -82,7 +83,18 @@ def complete_an_order():
                 with sr.AudioFile('./audio.wav') as source:
                     audio = r.record(source)
                 
-                GOOGLE_CLOUD_SPEECH_CREDENTIALS = r"""INSERT JSON OBJECT HERE"""
+                GOOGLE_CLOUD_SPEECH_CREDENTIALS = r"""{
+                    "type": "service_account",
+                    "project_id": "scap-hackathon",
+                    "private_key_id": "fee187bf48da677a28a178b8f7547aa7b13c3be1",
+                    "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCZFWUynwXwtNXm\ncyZoP623psUJLJ9gxgUUo05J1fHLA9g2nfz8hkMK1EpwylHb6SsRuuGNNbEwrt/S\nBnZ0YdGu/UlrQ0XhEHzeJbB5LYni6geoPX0iePaw3dj8ddxUQujRDUqxFMv31lma\n7B/vKkqPGDN1F1Tl5xThXriAt6ceHXFUGqy3FGusXZ1+f3xFlMT7ObKP05kGzIsH\nxAhOeGTPmbD/Evw7jOXePWG60mI7E5lmtZMRYJ7jRn9kF8cmbsLZ1dOWCUskI//Z\nvOy/mcBVOmfAGcwqn9NSHLWKw3TRYEk0XMmrMeBMqlxpGejSPGLaEG2AkIrI2iLk\nfRyY1L7tAgMBAAECggEACJB4XWklKW7MiryFeQ6a9uvXGSjMb9sUPK/YOOxFv0Bj\nT8bt+OjABnKTCZVgGAgG91J3vPE0jQ8ziWoAJYm1c+9KtLwKREmqWrlMErN4A+Bo\n9kIeZpfS4VlAAvvLDFHD+bIfLCIIOUDeOXsdXEGG81N8yX+5KbWQZv1oIBdmO5p+\n2EfvkJ7zyFM164zeNdKuY+G7k3rEotX7CRFpvNV7wuf2h9DYNFSlBk4NYiNbFxZb\nYLHq+uOsCrgyKwPq47Evn9ogqZcvUNB5d9/8CqF8R+xUanzOc4ijqje1OzPNQt5F\nRrpXf09/28QgFIuTxFObFC/BH+dxqA3Rs0mdhCX2kQKBgQDYTwEwHbbV9tBkBwUo\nr2A+sbpb7jsS0glFomkn5TKzJeVysqo3ztjpRnaQx9umP8kZPcckJywmhQlBREGa\noer04qhcKcml6IL1Mbf+fub/2OBWm/f8ulYw/dstK4O+ORveTBjctmtpE/CjGb8j\nF3EO5GYCr3a+szRUa//0O1lWZQKBgQC1LGyeqv6Hnu4VoVDMrbUn0H0JiJxSSWMI\nBQFPM1HfBQPGOJ+lPLYgWE27mvjiANhzK2Ovniis2LB0rMzKIbZeNrt7bB6gYWHV\nZKUbUHnaiRYkWtJSrdQuQtgtBF/DiOeCnJsMBBns4w5JyxoYlFTh9LMoebbtClac\ngYsrqcxZ6QKBgBahxYUMRtoCNfGRTnfgSZilrdL6jRG+ChxQDuKOt4xI9cXXbbnj\nGYzmw3cIgjHV0KukbBabRqJNHxfGFXshX9z5bhYehSuntTQEXLTzACZawbYuIXc+\n48/Cb9E9EU6w2PqcSammKHzZCj92bSo1xK2DNi0nwKvZQitnFWOvZfyFAoGBAIXj\n8836zqoJoSfpFJ5+zX9EC/PsuqHxTDeUA1i1s46e1SA7nnft+ybCO4gmykGzoELU\n0RHrT2IVrao8Pggvi+fqiPl4eNXLwhKDE6Ww/qyrXOq3F/I0NreNfNcgAWmvFnCn\nLcpeiY5QarHMYFfD30sxnOmPsclSN5nn/qLhFH1pAoGAZ8G1R0F8cBIW5PxIVbYB\nrGYnUyFcXvP1OXugkxOnxqZsy7TYlYOE0yIy29DvW+VyV//jmnVm4M1Loq1I01mv\n6jwODxl1QMwMvRnFVuLsFRQ2nDyB8BZRi2SQzBuCoCUMfxNL9t5XqTyPOaGBGKu+\nU7S/wo3eIkrBrwjcn6UKx2I=\n-----END PRIVATE KEY-----\n",
+                    "client_email": "scap-567@scap-hackathon.iam.gserviceaccount.com",
+                    "client_id": "112546786135886116997",
+                    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+                    "token_uri": "https://oauth2.googleapis.com/token",
+                    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+                    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/scap-567%40scap-hackathon.iam.gserviceaccount.com"
+                                    }"""
                 try:
                     transcription = r.recognize_google_cloud(audio, credentials_json=GOOGLE_CLOUD_SPEECH_CREDENTIALS)
                 except sr.UnknownValueError:
